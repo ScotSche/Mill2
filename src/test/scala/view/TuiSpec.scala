04652bed249c6f93ac4dcceddcb462e5e1c2ad6b
ScotSche <coding.scotsche@gmail.com>
2021-01-17 15:40:46 +0100

    Change structure of Tui Test

--- src/test/scala/view/TuiSpec.scala
+++ src/test/scala/view/TuiSpec.scala
@@ -6,9 +6,20 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 
-class TuiSpec extends AnyWordSpec with Matchers{
+class TuiSpec extends AnyWordSpec with Matchers {
 
   "A Mill Tui" should{
+    val controller = new Controller(new Board, Vector())
+    controller.create_new_Players("PlayerOne", "PlayerTwo")
+    val tui = new Tui(controller)
+
+    "create a new board on input 'n'" in {
+      tui.processInputLine("n")
+      controller.board should be(new Board)
+    }
+  }
+}
+/*"A Mill Tui" should{
     val controller = new Controller(new Board, Vector())
     controller.create_new_Players("PlayerOne", "PlayerTwo")
     val tui = new Tui(controller)
@@ -345,3 +356,4 @@ class TuiSpec extends AnyWordSpec with Matchers{
     }
   }
 }
+*/
